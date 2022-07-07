62f300bf388b9a128494f200bc05c01f503ee950
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-07-07 07:27:21 -0500

    recuperacvion archivos

--- Alquiler Maquinaria/Backend/src/main/java/com/alquiler/intrface/ClientInterface.java
+++
@@ -1,7 +0,0 @@
-package com.alquiler.intrface;
-
-import com.alquiler.model.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientInterface extends CrudRepository<Client,Integer> {
-}
--- Alquiler Maquinaria/Backend/src/main/java/com/alquiler/intrface/MessageInterface.java
+++
@@ -1,7 +0,0 @@
-package com.alquiler.intrface;
-
-import com.alquiler.model.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageInterface extends CrudRepository<Message,Integer>  {
-}
