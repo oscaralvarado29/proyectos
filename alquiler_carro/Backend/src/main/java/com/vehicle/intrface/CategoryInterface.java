e679a57d238cb5a96cf8ffac4735ca7abd564a68
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-08-12 06:47:52 -0500

    eliminacion configuracion anterior

--- alquiler_carro/Backend/src/main/java/com/vehicle/intrface/CategoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.vehicle.intrface;
-
-import com.vehicle.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryInterface extends CrudRepository<Category,Integer> {
-}
