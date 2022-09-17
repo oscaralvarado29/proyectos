3f53d08078b53b46725f2331ebb647232152eace
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-09-17 12:23:26 -0500

    confguracion de reportes con sonarlint y advertencas de iintelllij
    se configura la pagina de reportes.html y reportes.js para que cupla conlas ndcaciones de sonarlint y advertencas de iintelllij

--- Desarrollo Software/alquiler_carro/Front/js/reportes.js
+++ Desarrollo Software/alquiler_carro/Front/js/reportes.js
@@ -1,43 +1,24 @@
-function traerReporteDate() {
+function getReportDate() {
 
-    var fechaInicio = $("#rStartDate").val();
-    var fechaCierre = $("#rDevolutionDate").val();
-    console.log(fechaInicio);
-    console.log(fechaCierre);
+    let fechaInicio = $("#rStartDate").val();
+    let fechaCierre = $("#rDevolutionDate").val();
 
     $.ajax({
         //url: "http://10.0.1.5:8080/api/Reservation/report-dates/" + fechaInicio + "/" + fechaCierre,
         url:"http://localhost:8080/api/Reservation/report-dates/"+fechaInicio+"/"+fechaCierre,
         type: "GET",
         datatype: "JSON",
-        success: function (respuesta) {
-            console.log(respuesta);
-            pintarRespuestaDate(respuesta);
+        success: function (reportDate) {
+            drawReportDates(reportDate);
+        },
+        error: function (jqXHR, exception){
+            let msgError=generalFunctions(jqXHR, exception)
+            alert(msgError)
         }
     });
 }
 
-function  formatDate (date) {
-    let day = date.getDate();
-    let month = (date.getMonth() + 1);
-    let year = date.getFullYear();
-    let formatted_date;
-    let diaString;
-    let monthString;
-    if (day <10){
-        diaString = "0" + day.toString()
-    }else{
-        diaString = day.toString()
-    }
-    if (month <10){
-        monthString = "0" + month.toString()
-    }else{
-        monthString = month.toString()
-    }
-        formatted_date = diaString +"-" + monthString +"-" +year;
-    return formatted_date;
-}
-function pintarRespuestaDate(respuesta) {
+function drawReportDates(reportDate) {
 
     let myTable = "<table>";
     myTable += "<tr>";
@@ -48,19 +29,19 @@ function pintarRespuestaDate(respuesta) {
     myTable += "<th>Vehiculo</th>";
     myTable += "<th>Mensaje</th>";
     myTable += "<th>Califcación</th>";
-    "</tr>";
+    myTable += "</tr>";
 
-    for (i = 0; i < respuesta.length; i++) {
+    for (const element of reportDate) {
         
         myTable += "<tr>";
-        myTable += "<td>" + formatDate(new Date(respuesta[i].startDate)) + "</td>";
-        myTable += "<td>" + formatDate(new Date(respuesta[i].devolutionDate)) + "</td>";
-        myTable += "<td>" + respuesta[i].status + "</td>";
-        myTable += "<td>" + respuesta[i].client.name + "</td>";
-        myTable += "<td>" + respuesta[i].vehicle.name + "</td>";
-        if(respuesta[i].messages.length != 0){
-            myTable += "<td>" + respuesta[i].messages[0].messageText + "</td>";
-            myTable += "<td>" + respuesta[i].messages[0].score + "</td>";
+        myTable += "<td>" + formatDate(new Date(element.startDate),1) + "</td>";
+        myTable += "<td>" + formatDate(new Date(element.devolutionDate),1) + "</td>";
+        myTable += "<td>" + element.status + "</td>";
+        myTable += "<td>" + element.client.name + "</td>";
+        myTable += "<td>" + element.vehicle.name + "</td>";
+        if(element.messages.length !== 0){
+            myTable += "<td>" + element.messages[0].messageText + "</td>";
+            myTable += "<td>" + element.messages[0].score + "</td>";
         }else{
             myTable += "<td>" + "No hay mensajes" + "</td>";
             myTable += "<td>" + "No hay calificación" + "</td>";
@@ -71,58 +52,64 @@ function pintarRespuestaDate(respuesta) {
     $("#resultadoDate").html(myTable);
 }
 
-function traerReporteStatus() {
-    console.log("test");
+function getReportStatus() {
     $.ajax({
         //url: "http://10.0.1.5:8080/api/Reservation/report-status",
         url:"http://localhost:8080/api/Reservation/report-status",
         type: "GET",
         datatype: "JSON",
-        success: function (respuesta) {
-            console.log(respuesta);
-            pintarRespuesta(respuesta);
+        success: function (reportStatus) {
+            drawReportStatus(reportStatus);
+        },
+        error: function (jqXHR, exception){
+            let msgError=generalFunctions(jqXHR, exception)
+            alert(msgError)
         }
     });
 }
 
-function pintarRespuesta(respuesta) {
+function drawReportStatus(reportStatus) {
+
 
     let myTable = "<table>";
     myTable += "<tr>";
     myTable += "<th>completadas</th>";
-    myTable += "<td>" + respuesta.completed + "</td>";
+    myTable += "<td>" + reportStatus.completed + "</td>";
     myTable += "<th>canceladas</th>";
-    myTable += "<td>" + respuesta.cancelled + "</td>";
+    myTable += "<td>" + reportStatus.cancelled + "</td>";
     myTable += "</tr>";
     myTable += "</table>";
     $("#resultadoStatus").html(myTable);
 }
 
 
-function traerReporteClientes() {
+function getReportClients() {
     $.ajax({
         //url: "http://10.0.1.5:8080/api/Reservation/report-clients",
         url:"http://localhost:8080/api/Reservation/report-clients",
         type: "GET",
         datatype: "JSON",
-        success: function (respuesta) {
-            console.log(respuesta);
-            pintarRespuestaClientes(respuesta);
+        success: function (reportClients) {
+            drawReportClients(reportClients);
+        },
+        error: function (jqXHR, exception){
+            let msgError=generalFunctions(jqXHR, exception)
+            alert(msgError)
         }
     });
 }
 
-function pintarRespuestaClientes(respuesta) {
+function drawReportClients(reportClients) {
 
     let myTable = "<table>";
     myTable += "<th>Cantdad de reservas</th>";
     myTable += "<th>Cliente</th>";
     myTable += "<tr>";
 
-    for (i = 0; i < respuesta.length; i++) {
+    for (const element of reportClients) {
         
-        myTable += "<td>" + respuesta[i].total + "</td>";
-        myTable += "<td>" + respuesta[i].client.name + "</td>";
+        myTable += "<td>" + element.total + "</td>";
+        myTable += "<td>" + element.client.name + "</td>";
         myTable += "</tr>";
     }
     myTable += "</table>";
