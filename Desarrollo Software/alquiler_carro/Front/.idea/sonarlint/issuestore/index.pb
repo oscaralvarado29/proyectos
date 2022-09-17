977931c1f8f07ba10ff8ca1b2b3a45479ca38cbf
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-09-17 10:11:39 -0500

    confguracion de cliente con sonarlint y advertencas de iintelllij, creacion de errorAction.js
    se confiigura clientes.html y misFunciionesCliente.js para que cumplan con las ndcaciones de sonarlint y las advertencas de intelllij y se crea errorAction.js que contiiene la funcion de lo que debe hacer en caso de error

--- Desarrollo Software/alquiler_carro/Front/.idea/sonarlint/issuestore/index.pb
+++ Desarrollo Software/alquiler_carro/Front/.idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,9 @@
 
->
-Vehiculos.html,a\c\ac7c94131c0f2b14fa9ebb55c9e5a5e336798762
 B
 css/misestilos.css,b\d\bd0515f05387bc297128825d0c259a06cf150147
 =
 Mensajes.html,a\3\a3e69ae56140540a44273b238f239b4982da9326
 =
 Reportes.html,a\7\a788abdb2e2eebc35f70d6869afb8565a1234f0e
-=
-Clientes.html,1\c\1ce62a7f0b148aca192513a51cdb61e70d6a82a2
 B
 Reservaciones.html,8\4\84b09bbf90f2f93154296574213d9c6dd63a3d77
--- Desarrollo Software/alquiler_carro/Front/js/misFuncionesClientes.js
+++ Desarrollo Software/alquiler_carro/Front/js/misFuncionesClientes.js
@@ -1,18 +1,20 @@
-function traerInformacionClientes() {
-    console.log("se esta ejecutando")
+function getClient() {
     $.ajax({
         //url: "http://10.0.1.5:8080/api/Client/all",
         url: "http://localhost:8080/api/Client/all",
         type: "GET",
         datatype: "JSON",
-        success: function (respuesta) {
-            console.log(respuesta);
-            pintarCliente(respuesta);
+        success: function (clients) {
+            drawInfoClients(clients);
+        },
+        error: function (jqXHR, exception){
+            let msgError=errorAction(jqXHR, exception)
+            alert(msgError)
         }
     })
 }
 
-function pintarCliente(respuesta) {
+function drawInfoClients(clients) {
 
     let myTable = "<table>";
     myTable += "<th>Nombre</th>";
@@ -22,49 +24,46 @@ function pintarCliente(respuesta) {
     myTable += "<th> </th>";
     myTable += "<th> </th>";
     myTable += "<th> </th>";
-    "</tr>";
-    for (i = 0; i < respuesta.length; i++) {
+    myTable += "</tr>";
+    for (const element of clients) {
         myTable += "<tr>";
-        myTable += "<td>" + respuesta[i].name + "</td>";
-        myTable += "<td>" + respuesta[i].email + "</td>";
-        myTable += "<td>" + respuesta[i].age + "</td>";
-        if (typeof respuesta[i].messages.messageText === 'undefined'){
+        myTable += "<td>" + element.name + "</td>";
+        myTable += "<td>" + element.email + "</td>";
+        myTable += "<td>" + element.age + "</td>";
+        if (typeof element.messages.messageText === 'undefined'){
             myTable += "<td>" + "Ninguno" + "</td>";
         } else {
-            myTable += "<td>" + respuesta[i].messages.messageText + "</td>";
+            myTable += "<td>" + element.messages.messageText + "</td>";
         }
-        myTable += '<td><button  onclick="cargarDatosClientes(' + respuesta[i].idClient + ')">Lanzar</button></td>';
-        myTable += "<td> <button onclick=' actualizarInformacionCliente(" + respuesta[i].idClient + ")'>Actualizar</button>";
-        myTable += "<td> <button onclick='borrarCliente(" + respuesta[i].idClient + ")'>Borrar</button>";
+        myTable += '<td><button  onclick="launchDataClient(' + element.idClient + ')">Lanzar</button></td>';
+        myTable += "<td> <button onclick=' putClient(" + element.idClient + ")'>Actualizar</button>";
+        myTable += "<td> <button onclick='deleteClient(" + element.idClient + ")'>Borrar</button>";
         myTable += "</tr>";
     }
     myTable += "</table>";
     $("#mostrarClientes").html(myTable);
 }
 
-function cargarDatosClientes(id) {
+function launchDataClient(clientId) {
     $.ajax({
         dataType: 'json',
-        //url: "http://10.0.1.5:8080/api/Client/" + id,
-        url: "http://localhost:8080/api/Client/" + id,
+        //url: "http://10.0.1.5:8080/api/Client/" + clientId,
+        url: "http://localhost:8080/api/Client/" + clientId,
         type: 'GET',
 
         success: function (response) {
-            console.log(response);
-            var item = response;
-
-            $("#name").val(item.name);
-            $("#email").val(item.email);
-            $("#age").val(item.age);
+            $("#name").val(response.name);
+            $("#email").val(response.email);
+            $("#age").val(response.age);
         }
     });
 }
 
-function agregarCliente() {
-    if ($("#email").val()=="" || $("#name").val() == "" || $("#age").val()=="") {
+function postClient() {
+    if ($("#email").val() === "" || $("#name").val() === "" || $("#age").val() === "") {
         alert("Todos los campos son obligatorios")
     } else{
-        let datos = {
+        let dataClient = {
             email: $("#email").val(),
             name: $("#name").val(),
             age: $("#age").val(),
@@ -74,80 +73,79 @@ function agregarCliente() {
             type: 'POST',
             contentType: "application/json; charset=utf-8",
             dataType: 'JSON',
-            data: JSON.stringify(datos),
+            data: JSON.stringify(dataClient),
             //url: "http://10.0.1.5:8080/api/Client/save",
             url: "http://localhost:8080/api/Client/save",
             success: function (response) {
-                console.log(response);
-                alert("Se guardo correctamente");
-                window.location.reload()
+                $("#email").val(" ");
+                $("#name").val(" ");
+                $("#age").val(" ");
+                alert("El cliente "+response.name + " se guardo correctamente");
+
             },
 
-            error: function (jqXHR, textStatus, errorThrown) {
-                alert("No se guardo correctamente");
+            error: function (jqXHR, exception){
+                let msgError=errorAction(jqXHR, exception)
+                alert(msgError)
             }
         });
     }
 }
 
-function actualizarInformacionCliente(idElemento) {
-    if ($("#email").val() == "" || $("#name").val() == "" || $("#Clage").val() == "") {
+function putClient(idElemento) {
+    if ($("#email").val() === "" || $("#name").val() === "" || $("#Clage").val() === "") {
         alert("Todos los campos son obligatorios")
     } else{
-        let myData = {
+        let clientData = {
             idClient: idElemento,
             email: $("#email").val(),
             name: $("#name").val(),
             age: $("#age").val(),
         };
-        console.log(myData);
-        let dataToSend = JSON.stringify(myData);
         $.ajax({
             //url: "http://10.0.1.5:8080/api/Client/update",
             url: "http://localhost:8080/api/Client/update",
             type: "PUT",
-            data: dataToSend,
+            data:  JSON.stringify(clientData),
             contentType: "application/JSON",
             datatype: "JSON",
             success: function (respuesta) {
-                $("#email").val(" "),
-                    $("#name").val(" "),
-                    $("#age").val(" "),
-                    alert("se ha Actualizado correctamente ")
-                traerInformacionClientes()
+                $("#email").val(" ");
+                $("#name").val(" ");
+                $("#age").val(" ");
+                alert("Se ha actualizado correctamente el cliente "+respuesta.name)
+                getClient()
             },
-            error: function (jqXHR, textStatus, errorThrown) {
-                window.location.reload()
-                alert("No se actualizo correctamente");
+            error: function (jqXHR, exception){
+                let msgError=errorAction(jqXHR, exception)
+                alert(msgError)
             }
         });
 }
 }
 
-function borrarCliente(idElemento) {
-    let myData = {
+function deleteClient(idElemento) {
+    let dataClient = {
         idClient: idElemento
     };
-    let dataToSend = JSON.stringify(myData);
-    console.log(dataToSend);
     $.ajax({
         //url: "http://10.0.1.5:8080/api/Client/delete",
         url: "http://localhost:8080/api/Client/delete",
         type: "DELETE",
-        data: dataToSend,
+        data: JSON.stringify(dataClient),
         contentType: "application/JSON",
         datatype: "JSON",
-        success: function (respuesta) {
+        success: function () {
             $("#mostrarClientes").empty();
-            alert("Se ha eliminado correctamente")
-            $("#email").val(" "),
-            $("#name").val(" "),
-            $("#age").val(" "),
-            traerInformacionClientes();
+            alert("El cliente se ha eliminado correctamente")
+            $("#email").val(" ");
+            $("#name").val(" ");
+            $("#age").val(" ");
+            getClient();
         },
-        error: function (jqXHR, textStatus, errorThrown) {
-            window.location.reload()
-            alert("No se borro correctamente");
+        error: function (jqXHR, exception){
+            let msgError=errorAction(jqXHR, exception)
+            alert(msgError)
         }
     });
 
