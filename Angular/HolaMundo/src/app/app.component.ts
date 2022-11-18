06f0eb78663945e5ea04b458e3c8c4cf642cd7ea
oscaralvarado29 <oscar.alvarado.mt@correo.usa.edu.co>
2022-11-17 22:26:53 -0500

    sesion 1: confguracion inicial del proyecto

---
+++ Angular/HolaMundo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HolaMundo';
+  nombre = 'Oscar';
+}
