80fd89376ed26547f221880ad493e460c7e5f979
oscaralvarado29 <91513017+oscaralvarado29@users.noreply.github.com>
2023-01-19 18:32:41 -0500

    Delete Desarrollo_Software/alquiler_carro directory

--- Desarrollo_Software/alquiler_carro/Backend/src/main/java/com/vehicle/intrface/ClientInterface.java
+++
@@ -1,7 +0,0 @@
-package com.vehicle.intrface;
-
-import com.vehicle.model.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientInterface extends CrudRepository<Client,Integer> {
-}
--- Desarrollo_Software/alquiler_carro/Backend/src/main/java/com/vehicle/intrface/VehicleInterface.java
+++
@@ -1,7 +0,0 @@
-package com.vehicle.intrface;
-
-import com.vehicle.model.Vehicle;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VehicleInterface extends CrudRepository<Vehicle,Integer> {
-}
